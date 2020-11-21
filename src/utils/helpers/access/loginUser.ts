9bc0c1709d3c8c2328d985acca8edb595c121e87
Black Project Ventures <blackprojectventures@protonmail.com>
2020-11-22 01:28:33 +0530

    restores

--- src/utils/helpers/access/loginUser.ts
+++ src/utils/helpers/access/loginUser.ts
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const loginUser = (req: Request, res: Response, userId: string) => {
-  // req.session.userId = userId;
   let isLoggedCookieName: string = process.env.IS_AUTHENTICATED_COOKIE_NAME!;
   res.cookie(isLoggedCookieName, true, {
     expires: new Date(Date.now() + 31556952000),
