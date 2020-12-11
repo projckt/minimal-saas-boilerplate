5fea6e52750a81f84dcea617011df8f8272cede4
Black Project Ventures <blackprojectventures@protonmail.com>
2020-12-12 02:42:28 +0530

    changes names of global middlewares

--- src/global/middlewares/index.ts
+++ src/global/middlewares/index.ts
@@ -1,6 +1,6 @@
 // export { default as allowNonLoggedInUsers } from "./allowNonLoggedInUsers";
 
-export {} from "./";
+export { default as allowNonLoggedInUsers } from "./allowNonLoggedInUsers";
 export { default as allowNonSignedUpUsers } from "./allowNonSignedUpUsers";
 export { default as allowSignedUpUsers } from "./allowSignedUpUsers";
 export { default as allowLoggedInUsers } from "./allowLoggedInUsers";
